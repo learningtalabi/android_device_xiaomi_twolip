585225cb1d7faeed4bdeb088f83e911f3790322f
learningtalabi <80978880+learningtalabi@users.noreply.github.com>
2021-04-25 15:07:27 +0100

    orchid os update
    change lineage to aim for vendor

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_twolip.mk
+    $(LOCAL_DIR)/aim_twolip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_twolip-user \
-    lineage_twolip-userdebug \
-    lineage_twolip-eng
+    aim_twolip-user \
+    aim_twolip-userdebug \
+    aim_twolip-eng
