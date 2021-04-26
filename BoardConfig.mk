99816c0a6ec2a3ccb369da00feece915c7312737
the orchidproject <aosporchid@gmail.com>
2021-04-26 23:51:35 +0100

    aosp build for fusion

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@
 DEVICE_PATH := device/xiaomi/twolip
 
 # Orchidos by the projects
-BUILD_BROKEN_DUP_RULES := truedos
+BUILD_BROKEN_DUP_RULES := true
 
 
 #Assert
@@ -51,7 +51,7 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 2147483648
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-11-01
+VENDOR_SECURITY_PATCH := 2021-05-01
 
 # Inherit the proprietary files
 -include vendor/xiaomi/twolip/BoardConfigVendor.mk
